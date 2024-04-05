f64667445e635f98e0202cc2afb5f2fe14ea9a8b
Jari <jojanen@elisanet.fi>
2024-04-05 19:57:53 +0300

    make separate ORM layer from data classes

--- mqtt/data.hpp
+++ mqtt/data.hpp
@@ -6,26 +6,18 @@
 struct BaseData {
     long epoch;
 
-    virtual std::string insert() const = 0;
+    BaseData(long time) : epoch(time) {}
 };
 
 struct DataTemperature : public BaseData {
     float temperature;
     float humidity;
 
-    static std::string createTable() {
-        return "CREATE TABLE IF NOT EXISTS DataTemperature ("
-               "epoch INTEGER PRIMARY KEY,"
-               "temperature REAL,"
-               "humidity REAL);";
-    }
-
-    std::string insert() const override {
-        return "INSERT INTO DataTemperature (epoch, temperature, humidity) VALUES ("
-               + std::to_string(epoch) + ","
-               + std::to_string(temperature) + ","
-               + std::to_string(humidity) + ");";
-    }
+    DataTemperature(long time, float temp, float hum) :
+        BaseData(time),
+        temperature(temp), 
+        humidity(hum)
+    {}
 };
 
 struct DataWeather : public BaseData {
@@ -38,145 +30,37 @@ struct DataWeather : public BaseData {
     float uv;
     float solarRadiation;
 
-    static std::string createTable() {
-        return "CREATE TABLE IF NOT EXISTS DataWeather ("
-               "epoch INTEGER PRIMARY KEY,"
-               "temperature REAL,"
-               "humidity REAL,"
-               "pressure REAL,"
-               "windSpeed REAL,"
-               "windDirection REAL,"
-               "rain REAL,"
-               "uv REAL,"
-               "solarRadiation REAL);";
-    }
-
-    std::string insert() const override {
-        return "INSERT INTO DataWeather (epoch, temperature, humidity, pressure, windSpeed, windDirection, rain, uv, solarRadiation) VALUES ("
-               + std::to_string(epoch) + ","
-               + std::to_string(temperature) + ","
-               + std::to_string(humidity) + ","
-               + std::to_string(pressure) + ","
-               + std::to_string(windSpeed) + ","
-               + std::to_string(windDirection) + ","
-               + std::to_string(rain) + ","
-               + std::to_string(uv) + ","
-               + std::to_string(solarRadiation) + ");";
-    }
+    DataWeather(long time, float temp, float hum, float press, float windSpd, float windDir, float r, float uv, float solarRad) :
+        BaseData(time),
+        temperature(temp),
+        humidity(hum),
+        pressure(press),
+        windSpeed(windSpd),
+        windDirection(windDir),
+        rain(r),
+        uv(uv),
+        solarRadiation(solarRad)
+    {}
 };
+
 
 struct DataElPrice : public BaseData {
     float price;
 
-    static std::string createTable() {
-        return "CREATE TABLE IF NOT EXISTS DataElPrice ("
-               "epoch INTEGER PRIMARY KEY,"
-               "price REAL);";
-    }
-
-    std::string insert() const override {
-        return "INSERT INTO DataElPrice (epoch, price) VALUES ("
-               + std::to_string(epoch) + ","
-               + std::to_string(price) + ");";
-    }
+    DataElPrice(long time, float p) :
+        BaseData(time),
+        price(p)
+    {}
 };
+
 
 struct DataHistory : public BaseData {
     std::string device;
     int state;
 
-    static std::string createTable() {
-        return "CREATE TABLE IF NOT EXISTS DataHistory ("
-               "epoch INTEGER PRIMARY KEY,"
-               "device TEXT,"
-               "state INTEGER);";
-    }
-
-    std::string insert() const override {
-        return "INSERT INTO DataHistory (epoch, device, state) VALUES ("
-               + std::to_string(epoch) + ","
-               + "'" + device + "',"
-               + std::to_string(state) + ");";
-    }
-};
-
-class Database {
-public:
-    Database(const std::string& filename) {
-        sqlite3_open(filename.c_str(), &db);
-
-        std::string sql = DataTemperature::createTable() + \
-                          DataWeather::createTable() + \
-                          DataElPrice::createTable() + \
-                          DataHistory::createTable();    
-        sqlite3_exec(db, sql.c_str(), 0, 0, 0);
-    }
-
-    ~Database() {
-        sqlite3_close(db);
-    }
-
-    void insert(const DataTemperature& data) {
-        sqlite3_exec(db, data.insert().c_str(), 0, 0, 0);
-    }
-
-    void insert(const DataWeather& data) {
-        sqlite3_exec(db, data.insert().c_str(), 0, 0, 0);
-    }
-
-    void insert(const DataElPrice& data) {
-        sqlite3_exec(db, data.insert().c_str(), 0, 0, 0);
-    }
-
-    DataTemperature getTemperature(long epoch) {
-        DataTemperature data;
-        std::string sql = "SELECT * FROM DataTemperature WHERE epoch = " + std::to_string(epoch) + ";";
-        sqlite3_stmt* stmt;
-        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK) {
-            if (sqlite3_step(stmt) == SQLITE_ROW) {
-                data.epoch = sqlite3_column_int64(stmt, 0);
-                data.temperature = sqlite3_column_double(stmt, 1);
-                data.humidity = sqlite3_column_double(stmt, 2);
-            }
-            sqlite3_finalize(stmt);
-        }
-        return data;
-    }
-
-    DataWeather getWeather(long epoch) {
-        DataWeather data;
-        std::string sql = "SELECT * FROM DataWeather WHERE epoch = " + std::to_string(epoch) + ";";
-        sqlite3_stmt* stmt;
-        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK) {
-            if (sqlite3_step(stmt) == SQLITE_ROW) {
-                data.epoch = sqlite3_column_int64(stmt, 0);
-                data.temperature = sqlite3_column_double(stmt, 1);
-                data.humidity = sqlite3_column_double(stmt, 2);
-                data.pressure = sqlite3_column_double(stmt, 3);
-                data.windSpeed = sqlite3_column_double(stmt, 4);
-                data.windDirection = sqlite3_column_double(stmt, 5);
-                data.rain = sqlite3_column_double(stmt, 6);
-                data.uv = sqlite3_column_double(stmt, 7);
-                data.solarRadiation = sqlite3_column_double(stmt, 8);
-            }
-            sqlite3_finalize(stmt);
-        }
-        return data;
-    }
-
-    DataElPrice getElPrice(long epoch) {
-        DataElPrice data;
-        std::string sql = "SELECT * FROM DataElPrice WHERE epoch = " + std::to_string(epoch) + ";";
-        sqlite3_stmt* stmt;
-        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, NULL) == SQLITE_OK) {
-            if (sqlite3_step(stmt) == SQLITE_ROW) {
-                data.epoch = sqlite3_column_int64(stmt, 0);
-                data.price = sqlite3_column_double(stmt, 1);
-            }
-            sqlite3_finalize(stmt);
-        }
-        return data;
-    }
-private:
-    sqlite3* db;
+    DataHistory(long time, const std::string& dev, int st) :
+        BaseData(time),
+        device(dev),
+        state(st)
+    {}
 };
---
+++ mqtt/database.hpp
@@ -0,0 +1,221 @@
+#pragma once
+#include <data.hpp>
+#include <sqlite3.h>
+#include <vector>
+#include <memory>
+
+class DataTemperatureORM {
+public:
+    DataTemperatureORM(sqlite3* db) : db(db) {
+        const char* sql = "CREATE TABLE IF NOT EXISTS DataTemperature ("
+                          "epoch INTEGER PRIMARY KEY,"
+                          "temperature REAL,"
+                          "humidity REAL);";
+        sqlite3_exec(db, sql, 0, 0, 0);
+    }
+
+    void insert(const DataTemperature& data) {
+        const char* sql = "INSERT INTO DataTemperature (epoch, temperature, humidity) VALUES (?, ?, ?);";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+        sqlite3_bind_int64(stmt, 1, data.epoch);
+        sqlite3_bind_double(stmt, 2, data.temperature);
+        sqlite3_bind_double(stmt, 3, data.humidity);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+    }
+
+    std::vector<DataTemperature> getAll() {
+        const char* sql = "SELECT epoch, temperature, humidity FROM DataTemperature;";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+        std::vector<DataTemperature> data;
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            long epoch = sqlite3_column_int64(stmt, 0);
+            float temperature = sqlite3_column_double(stmt, 1);
+            float humidity = sqlite3_column_double(stmt, 2);
+            data.push_back(DataTemperature(epoch, temperature, humidity));
+        }
+
+        sqlite3_finalize(stmt);
+        return data;
+    }
+
+private:
+    sqlite3* db;
+};
+
+class DataWeatherORM {
+public:
+    DataWeatherORM(sqlite3* db) : db(db) {
+        const char* sql = "CREATE TABLE IF NOT EXISTS DataWeather ("
+                "epoch INTEGER PRIMARY KEY,"
+               "temperature REAL,"
+               "humidity REAL,"
+               "pressure REAL,"
+               "windSpeed REAL,"
+               "windDirection REAL,"
+               "rain REAL,"
+               "uv REAL,"
+               "solarRadiation REAL);";
+
+        sqlite3_exec(db, sql, 0, 0, 0);
+    }
+
+    void insert(const DataWeather& data) {
+        const char* sql = "INSERT INTO DataWeather (epoch, temperature, humidity, pressure, windSpeed, windDirection, rain, uv, solarRadiation) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+        sqlite3_bind_int64(stmt, 1, data.epoch);
+        sqlite3_bind_double(stmt, 2, data.temperature);
+        sqlite3_bind_double(stmt, 3, data.humidity);
+        sqlite3_bind_double(stmt, 4, data.pressure);
+        sqlite3_bind_double(stmt, 5, data.windSpeed);
+        sqlite3_bind_double(stmt, 6, data.windDirection);
+        sqlite3_bind_double(stmt, 7, data.rain);
+        sqlite3_bind_double(stmt, 8, data.uv);
+        sqlite3_bind_double(stmt, 9, data.solarRadiation);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+    }
+
+    std::vector<DataWeather> getAll() {
+        const char* sql = "SELECT epoch, temperature, humidity, pressure, windSpeed, windDirection, rain, uv, solarRadiation FROM DataWeather;";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+        std::vector<DataWeather> data;
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            long epoch = sqlite3_column_int64(stmt, 0);
+            float temperature = sqlite3_column_double(stmt, 1);
+            float humidity = sqlite3_column_double(stmt, 2);
+            float pressure = sqlite3_column_double(stmt, 3);
+            float windSpeed = sqlite3_column_double(stmt, 4);
+            float windDirection = sqlite3_column_double(stmt, 5);
+            float rain = sqlite3_column_double(stmt, 6);
+            float uv = sqlite3_column_double(stmt, 7);
+            float solarRadiation = sqlite3_column_double(stmt, 8);
+            data.push_back(DataWeather(epoch, temperature, humidity, pressure, windSpeed, windDirection, rain, uv, solarRadiation));
+        }
+
+        sqlite3_finalize(stmt);
+        return data;
+    }
+
+private:
+    sqlite3* db;
+};
+
+class DataElPriceORM {
+public:
+    DataElPriceORM(sqlite3* db) : db(db) {
+        const char* sql = "CREATE TABLE IF NOT EXISTS DataElPrice ("
+                          "epoch INTEGER PRIMARY KEY,"
+                          "price REAL);";
+        sqlite3_exec(db, sql, 0, 0, 0);
+    }
+
+    void insert(const DataElPrice& data) {
+        const char* sql = "INSERT INTO DataElPrice (epoch, price) VALUES (?, ?);";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+        sqlite3_bind_int64(stmt, 1, data.epoch);
+        sqlite3_bind_double(stmt, 2, data.price);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+    }
+
+    std::vector<DataElPrice> getAll() {
+        const char* sql = "SELECT epoch, price FROM DataElPrice;";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+        std::vector<DataElPrice> data;
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            long epoch = sqlite3_column_int64(stmt, 0);
+            float price = sqlite3_column_double(stmt, 1);
+            data.push_back(DataElPrice(epoch, price));
+        }
+
+        sqlite3_finalize(stmt);
+        return data;
+    }
+
+private:
+    sqlite3* db;
+};
+
+
+class DataHistoryORM {
+public:
+    DataHistoryORM(sqlite3* db) : db(db) {
+        const char* sql = "CREATE TABLE IF NOT EXISTS DataHistory ("
+                          "epoch INTEGER PRIMARY KEY,"
+                          "device TEXT,"
+                          "state INTEGER);";
+        sqlite3_exec(db, sql, 0, 0, 0);
+    }
+
+    void insert(const DataHistory& data) {
+        const char* sql = "INSERT INTO DataHistory (epoch, device, state) VALUES (?, ?, ?);";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+        sqlite3_bind_int64(stmt, 1, data.epoch);
+        sqlite3_bind_text(stmt, 2, data.device.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt, 3, data.state);
+        sqlite3_step(stmt);
+        sqlite3_finalize(stmt);
+    }
+
+    std::vector<DataHistory> getAll() {
+        const char* sql = "SELECT epoch, device, state FROM DataHistory;";
+        sqlite3_stmt* stmt;
+        sqlite3_prepare_v2(db, sql, -1, &stmt, 0);
+
+        std::vector<DataHistory> data;
+        while (sqlite3_step(stmt) == SQLITE_ROW) {
+            long epoch = sqlite3_column_int64(stmt, 0);
+            std::string device = reinterpret_cast<const char*>(sqlite3_column_text(stmt, 1));
+            int state = sqlite3_column_int(stmt, 2);
+            data.push_back(DataHistory(epoch, device, state));
+        }
+
+        sqlite3_finalize(stmt);
+        return data;
+    }
+
+private:
+    sqlite3* db;
+};
+
+
+class Database {
+public:
+    Database(sqlite3* db) : 
+        temperature(db),
+        weather(db),
+        elPrice(db),
+        history(db) 
+    {
+    }
+
+    ~Database() {
+        sqlite3_close(db);
+    }
+
+    DataTemperatureORM temperature;
+    DataWeatherORM weather;
+    DataElPriceORM elPrice;
+    DataHistoryORM history;
+
+private:
+    sqlite3* db;
+};
+
+std::unique_ptr<Database> createDatabase(const std::string& filename) {
+    sqlite3* db;
+
+    sqlite3_open(filename.c_str(), &db);
+    return std::unique_ptr<Database>(new Database(db));
+}
