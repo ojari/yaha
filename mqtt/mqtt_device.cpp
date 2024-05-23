4a03649be8a50ce27c635eac22a2e1acdc1c0fea
Jari <jojanen@elisanet.fi>
2024-05-23 19:26:14 +0300

    missing file from previous commit

--- mqtt/mqtt_device.cpp
+++ mqtt/mqtt_device.cpp
@@ -15,7 +15,7 @@ void DeviceRegistry::load(const std::string& filename) {
             std::string name = deviceData["name"].get<std::string>();
             const std::string type = deviceData["type"].get<std::string>();
 
-            devices_[name] = createDevice(type);
+            devices_[name] = createDevice(name, type);
             // std::cout << "Device: " << name << " Type: " << type << std::endl;
         }
 
@@ -26,10 +26,10 @@ void DeviceRegistry::load(const std::string& filename) {
 }
 
 
-std::shared_ptr<Device> DeviceRegistry::createDevice(const std::string& type) const {
+std::shared_ptr<Device> DeviceRegistry::createDevice(const std::string& name, const std::string& type) const {
     static const std::map<std::string, std::function<std::shared_ptr<Device>()>> deviceMap = {
-        {"Light", []() { return std::make_shared<LightDevice>(); }},
-        {"Switch", []() { return std::make_shared<SwitchDevice>(); }},
+        {"Light", [name]() { return std::make_shared<LightDevice>(name); }},
+        {"Switch", [name]() { return std::make_shared<SwitchDevice>(name); }},
         {"TempSensor", []() { return std::make_shared<TempSensorDevice>(); }}
     };
 
