10d708cf00d26170a69192269dc9dd3bb4b0706f
Jari <jojanen@elisanet.fi>
2024-08-25 13:21:28 +0300

    fix unit tests

--- test/test_logic.cpp
+++ test/test_logic.cpp
@@ -1,5 +1,6 @@
 #include "catch2/catch_all.hpp"
-#include "../mqtt/logic.hpp"
+#include "../mqtt/controller/lights.hpp"
+#include "../mqtt/controller/car_heater.hpp"
 #include "../mqtt/common.hpp"
 #include <memory>
 
@@ -15,7 +16,7 @@ std::shared_ptr<IActuator> actuator{new TestActuator()};
 
 
 TEST_CASE("Lights class test") {
-    Lights lights(actuator, hm2time(10, 0), hm2time(20, 0));
+    controller::Lights lights(actuator, hm2time(10, 0), hm2time(20, 0));
     SECTION("Turn on lights") {
         ValueItem item(ValueType::TIME, hm2time(12, 0));
         lights.onChange(item);
@@ -32,7 +33,7 @@ TEST_CASE("Lights class test") {
 
 TEST_CASE("CarHeaer class test") {
     int leaveTime = hm2time(10, 0);
-    CarHeater heater(actuator, leaveTime);
+    controller::CarHeater heater(actuator, leaveTime);
     auto data = GENERATE(
         std::make_pair(-21, 120),
         std::make_pair(-11,  90),
