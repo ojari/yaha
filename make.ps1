a5463bd5dab02ad25a2cff4a6d9c39f7ddcb0bc9
Jari <jojanen@elisanet.fi>
2024-12-27 17:32:31 +0200

    feat: add ARM support and refactor build script for improved clarity

--- make.ps1
+++ make.ps1
@@ -1,12 +1,21 @@
 param(
     [Parameter(Mandatory=$true)]
-    [ValidateSet("Build", "BuildArm", "Vcpkg", "VcpkgArm", "VcpkgWin", "Clean", "CleanArm", "Wc", "Run", "Export", "VsExport")]
-    [string]$Do
+    [ValidateSet("Build", "BuildArm", "Vcpkg", "VcpkgArm", "VcpkgWin", "Clean", "Wc", "Run", "Export", "VsExport")]
+    [string]$Do,
+    [bool]$Arm = $false
 )
 
-$CMAKE_GENERATOR="Ninja Multi-Config"
+$GENERATOR="Ninja Multi-Config"
 $VCPKG="/home/jari/vcpkg/vcpkg"
 #$VCPKG="C:/usr/vcpkg/vcpkg.exe"
+
+if ($Arm) {
+  Write-Host "ARM"
+  $bld_dir = "_build_arm"
+} else {
+  Write-Host "x64"
+  $bld_dir = "_build"
+}
 
 function doClean {
   param (
@@ -22,12 +31,16 @@ function doBuild {
       [string]$toolchain,
       [string]$bld_dir
   )
-  cmake --fresh -S"." -B"$bld_dir" --toolchain "etc/$toolchain" -G $CMAKE_GENERATOR -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON    
+  cmake --fresh -S"." -B"$bld_dir" `
+        --toolchain "etc/$toolchain" `
+        -G $GENERATOR `
+        -DCMAKE_BUILD_TYPE="Debug" `
+        -DCMAKE_EXPORT_COMPILE_COMMANDS=ON `
+        --log-level=NOTICE
   if ($LASTEXITCODE -ne 0) {
       throw "cmake config exit code: $LASTEXITCODE"
   }
 
-  Write-Host "cmake --build $bld_dir -j 10"
   cmake --build "$bld_dir" -j 10
   if ($LASTEXITCODE -ne 0) {
       throw "cmake build exit code: $LASTEXITCODE"
@@ -48,37 +61,42 @@ function doVsExport {
 }
 
 
-if ($Do -eq "Clean") {
-  doClean "_build"
-}
-if ($Do -eq "CleanArm") {
-  doClean "_build_arm"
-}
-if ($Do -eq "Build") {
-  doBuild "toolchain-x64.cmake" "_build"
-}
-if ($Do -eq "BuildArm") {
-  doBuild "toolchain-arm64.cmake" "_build_arm"
-}
-if ($Do -eq "Vcpkg") {
-  &"$VCPKG" install --triplet x64-linux-release
-}
-if ($Do -eq "VcpkgArm") {
-  &"$VCPKG" install --triplet arm64-linux-release
-}
-if ($Do -eq "VcpkgWin") {
-  &"$VCPKG" install --triplet x64-windows
-}
-if ($Do -eq "Wc") {
-  cloc --exclude-dir=expert --exclude-lang="CMake,Bourne Shell" mqtt weather spot
-}
-if ($Do -eq "Run") {
-  ./_build/mqtt/Debug/yaha
-}
-if ($Do -eq "Export") {
-    scp _build_arm/mqtt/yaha pi@${TARGET_HOST}:
-    scp *.json pi@${TARGET_HOST}:
-}
-if ($Do -eq "VsExport") {
-  doVsExport "toolchain-win.cmake" "vs"
+switch ($Do) {
+  "Clean" {
+    doClean $bld_dir
+  }
+  "Build" {
+    doBuild "toolchain-x64.cmake" "_build"
+  }
+  "BuildArm" {
+    doBuild "toolchain-arm64.cmake" "_build_arm"
+  }
+  "Vcpkg" {
+    &"$VCPKG" install --triplet x64-linux-release
+  }
+  "VcpkgArm" {
+    &"$VCPKG" install --triplet arm64-linux-release
+  }
+  "VcpkgWin" {
+    &"$VCPKG" install --triplet x64-windows
+  }
+  "Wc" {
+    cloc --exclude-dir=expert --exclude-lang="CMake,Bourne Shell" mqtt weather spot
+  }
+  "Run" {
+    ./_build/mqtt/Debug/yaha
+  }
+  "Export" {
+    $target = $env:TARGET_HOST
+    write-host "Export to $target"
+    scp _build_arm/mqtt/Debug/yaha pi@${target}:
+    scp etc/gen_devices.json pi@${target}:devices.json
+    scp etc/gen_automation.json pi@${target}:automation.json
+  }
+  "VsExport" {
+    doVsExport "toolchain-win.cmake" "vs"
+  }
+  default {
+    throw "Invalid option: $Do"
+  }
 }
