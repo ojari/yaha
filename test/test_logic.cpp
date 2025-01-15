3ab9236bbe7933a88f79fc2382e7ca3717732359
Jari <jojanen@elisanet.fi>
2025-01-15 22:30:30 +0200

    new unittests

--- test/test_logic.cpp
+++ test/test_logic.cpp
@@ -1,18 +1,22 @@
 #include "catch2/catch_all.hpp"
 #include "../mqtt/automation/lights.hpp"
+#include "../mqtt/automation/switch_light.hpp"
 #include "../mqtt/automation/car_heater.hpp"
 #include "../mqtt/common.hpp"
 #include <memory>
 
-class TestActuator : public IActuator {
-public:
+struct TestActuator : public IActuator {
     void set(std::string_view device, const std::string& value) override {
-        //this->device = device;
-        //this->value = value;
+        lastDevice = device;
+        lastValue = value;
     }
+
+    std::string lastDevice;
+    std::string lastValue;
 };
 
-std::shared_ptr<IActuator> actuator{new TestActuator()};
+TestActuator tactuator;
+std::shared_ptr<IActuator> actuator{&tactuator};
 
 
 TEST_CASE("Lights class test") {
@@ -30,6 +34,59 @@ TEST_CASE("Lights class test") {
     }
 }
 
+
+TEST_CASE("Switch mode 0") {
+    automation::SwitchLight sw(actuator, "test_dev");
+    sw.setArg("event", "Button Living Room");
+    sw.setArg("mode", "0");
+    sw.setArg("brightness", "64");
+
+    SECTION("Turn on - off switch") {
+        EventData item(EventId::BUTTON_LIVING_ROOM, 0);
+        sw.onChange(item);
+        REQUIRE(sw.get() == true);
+
+        sw.onChange(item);
+        REQUIRE(sw.get() == false);
+    }
+}
+
+TEST_CASE("Switch mode 1") {
+    automation::SwitchLight sw(actuator, "test_dev");
+    sw.setArg("event", "Button Living Room");
+    sw.setArg("mode", "1");
+    sw.setArg("brightness", "64");
+
+    SECTION("Turn on - off switch") {
+        EventData item(EventId::BUTTON_LIVING_ROOM, 1);
+        sw.onChange(item);
+        REQUIRE(sw.get() == true);
+
+        sw.onChange(item);
+        REQUIRE(sw.get() == false);
+    }
+}
+
+TEST_CASE("Switch mode 2") {
+    automation::SwitchLight sw(actuator, "test_dev");
+    sw.setArg("event", "Button Living Room");
+    sw.setArg("mode", "2");
+
+    SECTION("Turn on switch") {
+        sw.setArg("brightness", "40");
+
+        EventData item(EventId::BUTTON_LIVING_ROOM, 1);
+        sw.onChange(item);
+
+        REQUIRE(sw.get() == true);
+    }
+
+    SECTION("Turn off switch") {
+        EventData item(EventId::BUTTON_LIVING_ROOM, 0);
+        sw.onChange(item);
+        REQUIRE(sw.get() == false);
+    }
+}
 
 TEST_CASE("CarHeaer class test") {
     int leaveTime = hm2time(10, 0);
